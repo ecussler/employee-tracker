758918cd9258fad6f48463ed31df627987a277ce
E Cussler <cusslere@gmail.com>
2023-01-10 15:28:28 -0800

    Added donenv and established connection to local host

--- server.js
+++ server.js
@@ -1,4 +1,15 @@
 // Packages needed to run the application. 
-const fs = require('fs'); 
+const express = require('express'); 
 const inquirer = require('inquirer'); 
+const mysql = require('mysql2'); 
+const connection = require('./config/connection'); 
 inquirer.registerPrompt("loop", require("inquirer-loop")(inquirer)); 
+
+// const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+// Connect to database
