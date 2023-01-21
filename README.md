5bfd79c5a20301223ef006a148e820ce9f911590
E Cussler <cusslere@gmail.com>
2023-01-21 09:31:56 -0800

    Updated README file

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
-# employee-tracker
-An in-line application to view, add, and update employer departments and employees using Node.js, Inquirer, and MySQL.
+# Employee-Tracker [![License](https://img.shields.io/badge/License-MIT-yellow.svg)]
+  
+  ## Description
+  This inline application allows you to view and add company departments, roles, and employees. It additionally allows you to update employee roles, all using Node.js, Inquirer, and MySQL. 
+
+  This project what quite tricky for me. While this project does have some bugs and small display issues, the main functionality is there. 
+  
+  ## Table of Contents
+   - [Installation](#installation)
+   - [Usage](#usage)
+   - [Contribution](#contribution)
+   - [Testing](#testing)
+   - [License](#license)
+   - [Questions](#questions)
+
+  ## Installation
+  To install dependencies and run this application, please pass the following code in the command line of the application:
+
+  ~~~
+  npm i
+  ~~~
+
+  ## Usage
+  To start application, type node server into the command line. See below for the demonstration video. 
+
+  ![Application Walk-through Video](https://youtu.be/g9qWHLHZ45A)
+
+  ## Contribution
+  NA
+
+  ## Testing
+  To test, please pass the following code in the command line of the application:
+
+  ~~~
+  NA
+  ~~~
+
+  ## License
+  
+  This project uses the MIT license.
+
+  ## Questions
+  If you have any questions, please visit my GitHub or email me:
+  - GitHub profile: https://github.com/ecussler
+  - Email: cusslere@gmail.com
+  
--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 // Express middleware
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Connect to database
@@ -20,13 +20,7 @@ const db = mysql.createConnection(
   console.log(`Connected to the employees_db database.`)
 );
 
-
-db.query('SELECT * FROM department', function (err, results){
-    console.table(resluts); 
-})
-
-
-
+// Function to initialize inquirer
 function init() {
  inquirer.prompt([
     {
@@ -44,60 +38,188 @@ function init() {
         ]
     }
  ])
-    .then((response) => {
-        const [...choices] = response; 
-
-        if (choices === 'View All Departments') {
+    .then((response) => { 
+        console.log(response); 
+        const selected = response.options
+        if (selected === 'View All Departments') {
             viewAllDepartments();
-        } else if (choices === 'View All Roles') {
-            viewAllRoles(); 
-        } else if (choices === 'View All Employees') {
-            viewAllEmployees();
-        } else if (choices === 'Add a Department') {
-            addDepartment();
-        } else if (choices === 'Add a Role') {
-            addRole(); 
-        } else if (choices === 'Add an Employee') {
-            addEmployee();
-        } else if (choices === 'Update an Employee Role') {
-            updateRole();
         }
-});
+        else if (selected === 'View All Roles') {
+            viewAllRoles();
+            
+         } else if (selected === 'View All Employees') {
+             viewAllEmployees();
+         } else if (selected === 'Add a Department') {
+             addDepartment();
+         } else if (selected === 'Add a Role') {
+             addRole(); 
+         } else if (selected === 'Add an Employee') {
+             addEmployee();
+         } else if (selected === 'Update an Employee Role') {
+             updateRole();
+         }
+     })      
+    }
 
 
 // Show all departments
 function viewAllDepartments() {
-    db.query('SELECT * FROM department', (err, result) =>
-    err ? console.log(err) : console.table(result)
-    )
+    db.promise().query('SELECT * FROM department') 
+    .then((result) => console.table(result[0]))
+    .then(() => init()); 
 }
+
+
 
 // Show all roles
 function viewAllRoles() {
-    const query = `SELECT * FROM roles
-                   JOIN department ON roles.department_id = department.id`;
-    db.query(query, (err, result) =>
-    err ? console.log(err) : console.table(result)
-    )
+    db.promise().query(`SELECT * FROM roles JOIN department ON roles.department_id = department.id`) 
+    .then((result) => console.table(result[0]))
+    .then(() => init());
 }
 
+
+
 // Show all employees
-function viewAllEmployees()
+function viewAllEmployees() {
+
+    db.promise().query(`SELECT * FROM employee JOIN roles ON employee.role_id = roles.id`) 
+    .then((result) => console.table(result[0]))
+    .then(() => init());
+}
+
 
 // Add department
-function addDepartment()
+function addDepartment() {
+    inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'newDeptName', 
+            message: 'What is the name if your new department?', 
+        }
+    ])
+    .then((response) => {
+        const query = `INSERT INTO department (department_name)
+                       VALUES ("${response.newDeptName}")`; 
+        db.promise().query(query)
+        .then(() =>  console.log('Department added successfully!'))
+        .then(() => init()); 
+    })
+}
+
+
 
 // Add role
-function addRole()
+function addRole() {
+    db.query('SELECT * FROM department', (err, result) => {
 
-// Add employee
-function addEmployee()
+        inquirer.prompt([
+            {
+                type: 'input', 
+                name: 'newRoleName', 
+                message: 'What is the name if your new role?', 
+            }, 
+            {
+                type: 'input', 
+                name: 'salary', 
+                message: 'What is the salary? Please do not enter dollar signs or commas.',
+                default: '100000'
+            }, 
+            {
+                type: 'list', 
+                name: 'roleDept', 
+                message: 'What department does this role fall under? Please enter a number.',
+                choices: result.map(({ id, department_name }) => ({ name: department_name, value:id }))
+            }
+        ])
+        .then((response) => {
+            const query = `INSERT INTO roles (title, salary, department_id)
+                           VALUES ("${response.newRoleName}", ${response.salary}, ${response.roleDept})`; 
+            db.promise().query(query)
+            .then(() =>  console.log('Role added successfully!'))
+            .then( () => init()); 
+        })
+    })
+}
 
-// Update employee
-updateRole()
+
+function addEmployee() {
+    const employees =  db.promise().query('SELECT * FROM employee');
+    const roles =  db.promise().query('SELECT * FROM roles');
+    Promise.all([employees, roles]).then((values) => {
+
+
+    inquirer.prompt([
+        {
+            type: 'input', 
+            name: 'firstName', 
+            message: 'What is the first name of your new employee?', 
+        }, 
+        {
+            type: 'input', 
+            name: 'lastName', 
+            message: 'What is the last name of your new employee?',
+        }, 
+        {
+            type: 'list', 
+            name: 'employeeRole', 
+            message: 'What is the role of your new employee?',
+            choices: values[1][0].map(({ id, title }) => ({ name: title, value:id }))
+        },
+        {
+            type: 'list', 
+            name: 'manager', 
+            message: 'Does your employee have a manager?',
+            choices: values[0][0].map(({ id, first_name, last_name }) => ({ name: `${first_name} ${last_name}`, value:id }))
+
+        }
+    ])
+        .then((response) => {
+            console.log(response); 
+            const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+                           VALUES ('${response.firstName}', '${response.lastName}', ${response.employeeRole}, ${response.manager || null})`; 
+
+            db.promise().query(query)
+            .then(() =>  console.log('Employee added successfully!'))
+            .then( () => init()); 
+        })
+    })
+}
+
+
+// Update employee role
+function updateRole() {
+    const employees =  db.promise().query('SELECT * FROM employee');
+    const roles =  db.promise().query('SELECT * FROM roles');
+    Promise.all([employees, roles]).then((values) => {
+        
+        inquirer.prompt([
+            {
+                type: 'list', 
+                name: 'employeeName', 
+                message: 'Which employee would you like to update?', 
+                choices: values[0][0].map(({ id, first_name, last_name }) => ({ name: `${first_name} ${last_name}`, value:id }))
+            }, 
+            {
+                type: 'list', 
+                name: 'employeeRole', 
+                message: 'What is the new role of your new employee?',
+                choices: values[1][0].map(({ id, title }) => ({ name: title, value:id }))
+            }
+        ])
+            .then((response) => {
+                const query = `UPDATE employee SET role_id = "${response.employeeRole}"
+                           WHERE id = ${response.employeeName}`; 
+
+            db.promise().query(query)
+            .then(() =>  console.log('Employee updated successfully!'))
+            .then( () => init()); 
+            })
+    })
+}
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
-// init();
+init()
